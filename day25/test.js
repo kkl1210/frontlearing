7d74c4828fdf810e78b3fed55c7787fc140bb6eb
kelin <kelin.feng@oriente.com>
2018-08-17 18:13:38 +0800

    up

---
+++ day25/test.js
@@ -0,0 +1,6 @@
+let date = new Date();
+let hour = date.getHours()
+let m = date.getMinutes()
+console.log(date)
+console.log(typeof(hour))
+console.log(m)
---
+++ day34/bar.js
@@ -0,0 +1,25 @@
+function drawChart(datas) {
+    const chartHeight = 400
+    const space = 20
+    const columnColor = "#B0C4DE"
+    const aixsColor = black
+    //得到最大值
+    let max
+    max=datas[0]
+    for(let i =0;i<datas.length;i++){
+        if(max<datas[i]){
+            max =datas[i];
+        }
+    }
+    //数据和像素的转换
+    let maxHeight;
+    maxHeight = 20 * data
+    //绘制横轴和纵轴
+
+    //遍历数据
+    for(let i =0;i<sourceData[i].sale.length;i++){
+        
+    }
+
+
+}
